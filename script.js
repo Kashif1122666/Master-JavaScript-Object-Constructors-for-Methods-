d6467132f3a128364cc044efba71b09c6e960d12
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-06 01:21:09 -0700

    Add files via upload
    Master JavaScript Object Constructors for Methods!

---
+++ script.js
@@ -0,0 +1,39 @@
+// function Car(color, model, year){
+    // Properties
+    // this.color = color;
+    // this.model = model;
+    // this.year  = year;
+
+   // method to start the car
+//    this.start = function() {
+//     console.log(this.model + " is starting...");
+//    };
+
+   // Method to display the car's details
+//    this.displayDetails = function() {
+//        console.log("car: " + this.model + " , color: ", this.color + ", year: " + this.year);
+//    };
+// }
+
+
+  //   Objects of Car
+// var car1 = new Car("red", "Toyota", 2020);
+// var car2 = new Car("blue", "Honda", 2019);
+  //   Test the Methods
+// car1.start();
+// car2.displayDetails();
+
+
+// Using same method for different Objects  
+// function person(name , age) {
+//     this.name = name;
+//     this.age = age;
+//     this.greet = function() {
+//         console.log("Hello, my name is " + this.name + " and I am " + this.age + "years old.");
+//     };
+// }
+
+//     var person1 = new person("Alice",30);
+//     var person2 = new person("Bob",20);
+//     person1.greet(); //Hello, my name is Alice and I am 30years old.
+//     person2.greet(); // Hello, my name is Bob and I am 20years old.
